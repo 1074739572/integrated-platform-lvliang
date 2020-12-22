200317f70e7a6dd6660a0311d7aef9107a3624cd
czzhan <czzhan@iflytek>
2020-12-22 10:34:53 +0800

    全局异常统一返回的格式

--- platform/src/main/java/com/iflytek/integrated/platform/exception/GlobalExceptionHandler.java
+++ platform/src/main/java/com/iflytek/integrated/platform/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.iflytek.integrated.platform.exception;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.iflytek.integrated.common.ResultDto;
 import com.iflytek.medicalboot.core.dto.Response;
 import com.iflytek.medicalboot.core.exception.MedicalBusinessException;
 import com.iflytek.medicalboot.core.exception.MedicalFatalException;
@@ -9,8 +10,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
@@ -37,7 +36,7 @@ import java.util.stream.Collectors;
 @ControllerAdvice
 public class GlobalExceptionHandler {
     private static final Logger logger = LoggerFactory.getLogger(com.iflytek.medicalboot.core.exception.GlobalExceptionHandler.class);
-    public static final String ERROR_CODE = "500";
+    public static final Integer ERROR_CODE = 500;
     public static final String ERROR_MSG = "系统异常，请联系管理员。";
 
     @Value("${medical.isPrintStackTrace:true}")
@@ -53,69 +52,61 @@ public class GlobalExceptionHandler {
 
     @ResponseBody
     @ExceptionHandler(value = {Exception.class})
-    public ResponseEntity<Object> exceptionHandler(Exception e) {
-        Response<Object> resp = null;
-        HttpStatus status = HttpStatus.BAD_REQUEST;
+    public ResultDto exceptionHandler(Exception e) {
+        ResultDto resultDto = new ResultDto(ERROR_CODE,ERROR_MSG,"");
         if (e instanceof MedicalBusinessException) {
-            resp = ((MedicalBusinessException) e).getResponse();
+            resultDto.setData(((MedicalBusinessException) e).getResponse());
         } else if (e instanceof MedicalHttpStatusCodeException) {
             MedicalHttpStatusCodeException statusCodeException = (MedicalHttpStatusCodeException) e;
-            resp = statusCodeException.getResponse();
-            status = HttpStatus.valueOf(statusCodeException.getStatusCode());
+            resultDto.setData(statusCodeException.getResponse());
         } else if (e instanceof MedicalFatalException) {
             // 监控平台 配置监控MedicalFatalException并告警
-            resp = new Response<>(ERROR_CODE, ERROR_MSG, getStackTrace(e));
-            status = HttpStatus.INTERNAL_SERVER_ERROR;
+            resultDto.setData(getStackTrace(e));
         } else if (e instanceof HttpStatusCodeException) {
             HttpStatusCodeException ex = (HttpStatusCodeException) e;
             String body = ex.getResponseBodyAsString();
             // RestTemplate调用错误, 尽可能将上级错误信息返回
             if (StringUtils.hasText(body)) {
                 try {
-                    resp = objectMapper.readValue(body, Response.class);
+                    resultDto.setData(objectMapper.readValue(body, Response.class));
                 } catch (Exception exception) {
-                    resp = new Response<>(Integer.toString(ex.getRawStatusCode()), body, getStackTrace(ex));
+                    resultDto.setData(getStackTrace(ex));
                 }
             } else {
-                resp = new Response<>(Integer.toString(ex.getRawStatusCode()), ex.getMessage(), getStackTrace(ex));
+                resultDto.setData(getStackTrace(ex));
             }
-            status = HttpStatus.valueOf(ex.getRawStatusCode());
         } else if (e instanceof MethodArgumentNotValidException) {
             // BeanValidation exception
             MethodArgumentNotValidException ex = (MethodArgumentNotValidException) e;
-            resp = new Response<>(Integer.toString(HttpStatus.BAD_REQUEST.value()), ERROR_MSG, getStackTrace(ex));
+            resultDto.setData(getStackTrace(ex));
             if (isPrintBeanValidation) {
-                setValidationErrorsMsg(resp, ex.getBindingResult());
+                setValidationErrorsMsg(resultDto, ex.getBindingResult());
             }
         } else if (e instanceof ConstraintViolationException) {
             // BeanValidation GET simple param
             ConstraintViolationException ex = (ConstraintViolationException) e;
-            resp = new Response<>(Integer.toString(HttpStatus.BAD_REQUEST.value()), ERROR_MSG, getStackTrace(ex));
+            resultDto.setData(getStackTrace(ex));
             if (isPrintBeanValidation) {
-                setValidationConstraintViolationMsg(resp, ex.getConstraintViolations());
+                setValidationConstraintViolationMsg(resultDto, ex.getConstraintViolations());
             }
         } else if (e instanceof BindException) {
             // BeanValidation GET object param
             BindException ex = (BindException) e;
-            resp = new Response<>(Integer.toString(HttpStatus.BAD_REQUEST.value()), ERROR_MSG, getStackTrace(ex));
+            resultDto.setData(getStackTrace(ex));
             if (isPrintBindException) {
-                setValidationErrorsMsg(resp, ex.getBindingResult());
+                setValidationErrorsMsg(resultDto, ex.getBindingResult());
             }
         } else if(e instanceof RuntimeException){
             //如果是后端抛出的错误，返回抛出的原因
-            resp = new Response<>(ERROR_CODE, e.getMessage(), null);
+            resultDto.setData(e.getMessage());
         }
         else {
-            resp = new Response<>(ERROR_CODE, ERROR_MSG, getStackTrace(e));
-            status = HttpStatus.INTERNAL_SERVER_ERROR;
-            if (isPrintStackTrace) {
-                resp.setMsg(e.getMessage());
-            }
+            resultDto.setData(e.getMessage());
         }
-        return new ResponseEntity(resp, status);
+        return resultDto;
     }
 
-    private void setValidationConstraintViolationMsg(Response<Object> resp, Set<ConstraintViolation<?>> constraintViolations) {
+    private void setValidationConstraintViolationMsg(ResultDto resultDto, Set<ConstraintViolation<?>> constraintViolations) {
         if (isPrintValidationJson) {
             try {
                 List<AbstractMap.SimpleEntry<String, String>> errors = constraintViolations.stream()
@@ -129,28 +120,28 @@ public class GlobalExceptionHandler {
                         })
                         .collect(Collectors.toList());
 
-                resp.setMsg(objectMapper.writeValueAsString(errors));
+                resultDto.setMessage(objectMapper.writeValueAsString(errors));
             } catch (Exception exc) {
                 logger.error("setValidationConstraintViolationMsg", exc);
             }
         } else {
-            resp.setMsg(constraintViolations.stream().map(ConstraintViolation::getMessage).collect(Collectors.joining(", ")));
+            resultDto.setMessage(constraintViolations.stream().map(ConstraintViolation::getMessage).collect(Collectors.joining(", ")));
         }
     }
 
-    private void setValidationErrorsMsg(Response<Object> resp, BindingResult bindingResult) {
+    private void setValidationErrorsMsg(ResultDto resp, BindingResult bindingResult) {
         if (isPrintValidationJson) {
             try {
                 List<AbstractMap.SimpleEntry<String, String>> errors = bindingResult.getAllErrors().stream()
                         .map(oe -> new HashMap.SimpleEntry<>(((FieldError) oe).getField(), oe.getDefaultMessage()))
                         .collect(Collectors.toList());
 
-                resp.setMsg(objectMapper.writeValueAsString(errors));
+                resp.setMessage(objectMapper.writeValueAsString(errors));
             } catch (Exception exc) {
                 logger.error("setValidationErrorsMsg", exc);
             }
         } else {
-            resp.setMsg(bindingResult.getAllErrors().stream().map(ObjectError::getDefaultMessage).collect(Collectors.joining(", ")));
+            resp.setMessage(bindingResult.getAllErrors().stream().map(ObjectError::getDefaultMessage).collect(Collectors.joining(", ")));
         }
     }
 
