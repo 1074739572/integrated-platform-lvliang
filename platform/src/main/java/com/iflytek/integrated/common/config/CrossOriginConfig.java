210141766e1c1e270861e73e91b20770eaf454ed
czzhan <czzhan@iflytek>
2020-12-24 10:41:45 +0800

    全局跨域

---
+++ platform/src/main/java/com/iflytek/integrated/common/config/CrossOriginConfig.java
@@ -0,0 +1,36 @@
+package com.iflytek.integrated.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * 全局跨域
+ * @author czzhan
+ * @version 1.0
+ * @date 2020/12/24 10:32
+ */
+@Configuration
+public class CrossOriginConfig {
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        //允许任何域名
+        corsConfiguration.addAllowedOrigin("*");
+        //允许任何头
+        corsConfiguration.addAllowedHeader("*");
+        //允许任何方法
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        //注册
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+}
