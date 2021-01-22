d4651463cb536d7c07da4460b8bda3ff71c44484
czzhan <czzhan@iflytek>
2021-01-22 19:56:25 +0800

    打印sql

--- platform/src/main/java/com/iflytek/integrated/common/advice/SqlCloseListener.java
+++ platform/src/main/java/com/iflytek/integrated/common/advice/SqlCloseListener.java
@@ -1,6 +1,8 @@
 package com.iflytek.integrated.common.advice;
 
 import com.querydsl.sql.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
 
@@ -10,12 +12,24 @@ import javax.sql.DataSource;
  * @date 2021/1/22 19:20
  */
 public class SqlCloseListener extends SpringSqlCloseListener {
+    private static final Logger log = LoggerFactory.getLogger(SqlCloseListener.class);
 
     public SqlCloseListener(DataSource dataSource) {
         super(dataSource);
     }
 
+    /**
+     * 重写了SpringSqlCloseListener的此方法，不再打印
+     * @param context
+     */
     @Override
     public void preExecute(SQLListenerContext context) {
+        if (log.isDebugEnabled()) {
+            SQLBindings sqlBindings = context.getSQLBindings();
+            if (sqlBindings != null && sqlBindings.getNullFriendlyBindings() != null) {
+                log.debug("SQL: {}", sqlBindings.getSQL());
+                log.debug("Bindings: {}", sqlBindings.getNullFriendlyBindings().toString());
+            }
+        }
     }
 }
