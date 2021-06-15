f83919be3289fc5b24e635ad00abf9efee71c237
snli6 <lixiaolonng233@126.com>
2021-06-15 16:26:32 +0800

    webservice服务驱动及schema缓存key获取

--- platform/src/main/java/com/iflytek/integrated/platform/common/RedisService.java
+++ platform/src/main/java/com/iflytek/integrated/platform/common/RedisService.java
@@ -1,30 +1,5 @@
 package com.iflytek.integrated.platform.common;
 
-import com.iflytek.integrated.common.dto.ResultDto;
-import com.iflytek.integrated.common.utils.JackSonUtils;
-import com.iflytek.integrated.common.utils.RedisUtil;
-import com.iflytek.integrated.platform.dto.RedisDto;
-import com.iflytek.integrated.platform.dto.RedisKeyDto;
-import com.querydsl.core.types.Predicate;
-import com.querydsl.core.types.Projections;
-import com.querydsl.core.types.dsl.StringPath;
-import com.querydsl.sql.SQLQueryFactory;
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import springfox.documentation.annotations.ApiIgnore;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-
 import static com.iflytek.integrated.platform.entity.QTBusinessInterface.qTBusinessInterface;
 import static com.iflytek.integrated.platform.entity.QTDrive.qTDrive;
 import static com.iflytek.integrated.platform.entity.QTHospital.qTHospital;
@@ -36,6 +11,33 @@ import static com.iflytek.integrated.platform.entity.QTSys.qTSys;
 import static com.iflytek.integrated.platform.entity.QTSysConfig.qTSysConfig;
 import static com.iflytek.integrated.platform.entity.QTSysDriveLink.qTSysDriveLink;
 import static com.iflytek.integrated.platform.entity.QTSysHospitalConfig.qTSysHospitalConfig;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.iflytek.integrated.common.dto.ResultDto;
+import com.iflytek.integrated.common.utils.JackSonUtils;
+import com.iflytek.integrated.common.utils.RedisUtil;
+import com.iflytek.integrated.platform.dto.RedisDto;
+import com.iflytek.integrated.platform.dto.RedisKeyDto;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.StringPath;
+import com.querydsl.sql.SQLQueryFactory;
+
+import springfox.documentation.annotations.ApiIgnore;
 
 /**
  * redis缓存操作
@@ -124,21 +126,58 @@ public class RedisService {
 
 		List<RedisKeyDto> list = sqlQueryFactory
 				.select(Projections.bean(RedisKeyDto.class, qTSysHospitalConfig.hospitalCode.as("orgId"),
-						qTSys.sysCode.as("sysCode"), qTInterface.interfaceUrl.as("funCode"))).from(qTProject)
-				.leftJoin(qTPlatform).on(qTPlatform.projectId.eq(qTProject.id))
-				.leftJoin(qTSysConfig).on(qTSysConfig.platformId.eq(qTPlatform.id))
-				.leftJoin(qTSys).on(qTSysConfig.sysId.eq(qTSys.id))
-				.leftJoin(qTInterface).on(qTSys.id.eq(qTInterface.sysId))
-				.leftJoin(qTBusinessInterface)
+						qTSys.sysCode.as("sysCode"), qTInterface.interfaceUrl.as("funCode")))
+				.from(qTProject).leftJoin(qTPlatform).on(qTPlatform.projectId.eq(qTProject.id)).leftJoin(qTSysConfig)
+				.on(qTSysConfig.platformId.eq(qTPlatform.id)).leftJoin(qTSys).on(qTSysConfig.sysId.eq(qTSys.id))
+				.leftJoin(qTInterface).on(qTSys.id.eq(qTInterface.sysId)).leftJoin(qTBusinessInterface)
 				.on(qTBusinessInterface.requestInterfaceId.eq(qTInterface.id)
 						.and(qTBusinessInterface.requestSysconfigId.eq(qTSysConfig.id)
 								.or(qTBusinessInterface.requestedSysconfigId.eq(qTSysConfig.id))))
 				.leftJoin(qTSysHospitalConfig).on(qTSysConfig.id.eq(qTSysHospitalConfig.sysConfigId))
-				.leftJoin(qTHospital).on(qTSysHospitalConfig.hospitalId.eq(qTHospital.id))
-				.leftJoin(qTSysDriveLink).on(qTSysDriveLink.sysId.eq(qTSys.id))
-				.leftJoin(qTDrive).on(qTDrive.id.eq(qTSysDriveLink.driveId))
+				.leftJoin(qTHospital).on(qTSysHospitalConfig.hospitalId.eq(qTHospital.id)).leftJoin(qTSysDriveLink)
+				.on(qTSysDriveLink.sysId.eq(qTSys.id)).leftJoin(qTDrive).on(qTDrive.id.eq(qTSysDriveLink.driveId))
 				.leftJoin(qTPlugin).on(qTPlugin.id.eq(qTBusinessInterface.pluginId))
 				.where(arr.toArray(new Predicate[arr.size()]))
+				.groupBy(qTSys.sysCode, qTSysHospitalConfig.hospitalCode, qTInterface.interfaceUrl).fetch();
+		return list;
+	}
+
+	/**
+	 * 删除key操作
+	 * 
+	 * @param arr
+	 */
+	public List<RedisKeyDto> getWsDriverRedisKeyDtoList(ArrayList<Predicate> arr) {
+		arr.add(qTSysConfig.sysConfigType.eq(1).and(qTSysHospitalConfig.hospitalCode.isNotNull())
+				.and(qTSys.sysCode.isNotNull()));
+
+		List<RedisKeyDto> list = sqlQueryFactory
+				.select(Projections.bean(RedisKeyDto.class, qTSysHospitalConfig.hospitalCode.as("orgId"),
+						qTSys.sysCode.as("sysCode")))
+				.from(qTSysDriveLink).join(qTDrive).on(qTSysDriveLink.driveId.eq(qTDrive.id)).join(qTSys)
+				.on(qTSysDriveLink.sysId.eq(qTSys.id)).join(qTSysConfig).on(qTSysConfig.sysId.eq(qTSysDriveLink.sysId))
+				.join(qTSysHospitalConfig).on(qTSysConfig.id.eq(qTSysHospitalConfig.sysConfigId)).join(qTHospital)
+				.on(qTHospital.id.eq(qTSysHospitalConfig.hospitalId)).where(arr.toArray(new Predicate[arr.size()]))
+				.groupBy(qTSys.sysCode, qTSysHospitalConfig.hospitalCode).fetch();
+		return list;
+	}
+
+	/**
+	 * 删除key操作
+	 * 
+	 * @param arr
+	 */
+	public List<RedisKeyDto> getWsSchemaRedisKeyDtoList(ArrayList<Predicate> arr) {
+		arr.add(qTSysConfig.sysConfigType.eq(1).and(qTSysHospitalConfig.hospitalCode.isNotNull())
+				.and(qTSys.sysCode.isNotNull().and(qTInterface.interfaceUrl.isNotNull())));
+
+		List<RedisKeyDto> list = sqlQueryFactory
+				.select(Projections.bean(RedisKeyDto.class, qTSysHospitalConfig.hospitalCode.as("orgId"),
+						qTSys.sysCode.as("sysCode"), qTInterface.interfaceUrl.as("funCode")))
+				.from(qTInterface).join(qTSys).on(qTInterface.sysId.eq(qTSys.id)).join(qTSysConfig)
+				.on(qTSysConfig.sysId.eq(qTSys.id)).join(qTSysHospitalConfig)
+				.on(qTSysConfig.id.eq(qTSysHospitalConfig.sysConfigId)).join(qTHospital)
+				.on(qTHospital.id.eq(qTSysHospitalConfig.hospitalId)).where(arr.toArray(new Predicate[arr.size()]))
 				.groupBy(qTSys.sysCode, qTSysHospitalConfig.hospitalCode, qTInterface.interfaceUrl).fetch();
 		return list;
 	}
@@ -157,4 +196,20 @@ public class RedisService {
 		logger.info("删除缓存{}条", list.size());
 	}
 
+	public void delWsDriverRedisKey(List<RedisKeyDto> list) {
+		for (RedisKeyDto obj : list) {
+			String key = "IntegratedPlatform:Configs:WS:drivers:_" + obj.getSysCode() + "_" + obj.getOrgId();
+			redisUtil.del(key);
+		}
+		logger.info("删除webservice驱动缓存{}条", list.size());
+	}
+
+	public void delWsSchemaRedisKey(List<RedisKeyDto> list) {
+		for (RedisKeyDto obj : list) {
+			String key = "IntegratedPlatform:Configs:WS:schema:_" + obj.getFunCode() + "_" + obj.getSysCode() + "_"
+					+ obj.getOrgId();
+			redisUtil.del(key);
+		}
+		logger.info("删除webservicet入参schema缓存{}条", list.size());
+	}
 }
