3c59fa18d09e5c72a028930826bd70d43df05156
snli6 <lixiaolonng233@126.com>
2021-08-31 14:05:01 +0800

    重放接口修改

--- platform/src/main/java/com/iflytek/integrated/platform/service/LogService.java
+++ platform/src/main/java/com/iflytek/integrated/platform/service/LogService.java
@@ -1,5 +1,36 @@
 package com.iflytek.integrated.platform.service;
 
+import static com.iflytek.integrated.platform.entity.QTBusinessInterface.qTBusinessInterface;
+import static com.iflytek.integrated.platform.entity.QTInterface.qTInterface;
+import static com.iflytek.integrated.platform.entity.QTInterfaceMonitor.qTInterfaceMonitor;
+import static com.iflytek.integrated.platform.entity.QTLog.qTLog;
+import static com.iflytek.integrated.platform.entity.QTPlatform.qTPlatform;
+import static com.iflytek.integrated.platform.entity.QTProject.qTProject;
+import static com.iflytek.integrated.platform.entity.QTSys.qTSys;
+import static com.iflytek.integrated.platform.entity.QTSysConfig.qTSysConfig;
+
+import java.io.OutputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.iflytek.integrated.common.dto.ResultDto;
 import com.iflytek.integrated.common.dto.TableData;
 import com.iflytek.integrated.common.utils.ExceptionUtil;
@@ -7,10 +38,8 @@ import com.iflytek.integrated.common.utils.SensitiveUtils;
 import com.iflytek.integrated.common.utils.ase.AesUtil;
 import com.iflytek.integrated.platform.common.BaseService;
 import com.iflytek.integrated.platform.common.Constant;
-import com.iflytek.integrated.platform.dto.InterfaceDebugDto;
 import com.iflytek.integrated.platform.dto.InterfaceMonitorDto;
 import com.iflytek.integrated.platform.entity.QTInterfaceMonitor;
-import com.iflytek.integrated.platform.entity.TBusinessInterface;
 import com.iflytek.integrated.platform.entity.TLog;
 import com.iflytek.integrated.platform.utils.NiFiRequestUtil;
 import com.iflytek.integrated.platform.utils.PlatformUtil;
@@ -22,36 +51,11 @@ import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.core.types.dsl.NumberPath;
 import com.querydsl.core.types.dsl.StringPath;
 import com.querydsl.sql.SQLExpressions;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.OutputStream;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.iflytek.integrated.platform.entity.QTBusinessInterface.qTBusinessInterface;
-import static com.iflytek.integrated.platform.entity.QTHospital.qTHospital;
-import static com.iflytek.integrated.platform.entity.QTInterface.qTInterface;
-import static com.iflytek.integrated.platform.entity.QTInterfaceMonitor.qTInterfaceMonitor;
-import static com.iflytek.integrated.platform.entity.QTLog.qTLog;
-import static com.iflytek.integrated.platform.entity.QTPlatform.qTPlatform;
-import static com.iflytek.integrated.platform.entity.QTProject.qTProject;
-import static com.iflytek.integrated.platform.entity.QTSys.qTSys;
-import static com.iflytek.integrated.platform.entity.QTSysConfig.qTSysConfig;
-import static com.iflytek.integrated.platform.entity.QTSysHospitalConfig.qTSysHospitalConfig;
 
 /**
  * @author czzhan
@@ -424,106 +428,23 @@ public class LogService extends BaseService<TLog, Long, NumberPath<Long>> {
 		}
 
 		String[] idArrays = ids.split(",");
-		String id = idArrays[0];
-//		int successCount = 0;
-//		int errorConut = 0;
-//		String errorIds = "";
-//		for(String id : idArrays){
-			//请求方接口调试数据获取
+		if(idArrays != null && idArrays.length > 0) {
 			try {
-				List<TBusinessInterface> businessInterfaces = sqlQueryFactory
-						.select(Projections.bean(TBusinessInterface.class, qTBusinessInterface.id,
-								qTBusinessInterface.requestInterfaceId, qTBusinessInterface.requestSysconfigId,
-								qTProject.projectCode.as("projectCode"), qTInterface.interfaceUrl.as("interfaceUrl"),
-								qTSys.sysCode.as("sysCode"), qTInterface.inParamFormatType.as("sysIntfInParamFormatType"),
-								qTInterface.sysId.as("requestSysId")))
-						.from(qTBusinessInterface).leftJoin(qTInterface)
-						.on(qTBusinessInterface.requestInterfaceId.eq(qTInterface.id)).leftJoin(qTSysConfig)
-						.on(qTSysConfig.id.eq(qTBusinessInterface.requestSysconfigId)).leftJoin(qTPlatform)
-						.on(qTPlatform.id.eq(qTSysConfig.platformId)).leftJoin(qTProject)
-						.on(qTProject.id.eq(qTPlatform.projectId)).leftJoin(qTSys).on(qTSys.id.eq(qTSysConfig.sysId))
-						.where(qTBusinessInterface.id.eq(id)).fetch();
-				if (businessInterfaces == null || businessInterfaces.size() == 0) {
-					return new ResultDto<>(Constant.ResultCode.ERROR_CODE, "日志重放失败,接口转换id在数据库不存在");
+				Long[] realIds = new Long[idArrays.length];
+				for(int i = 0 ; i < idArrays.length ; i++) {
+					realIds[i] = Long.valueOf(idArrays[i]);
 				}
-				// 获取入参列表
-				TBusinessInterface businessInterface = businessInterfaces.get(0);
-				String interfaceId = StringUtils.isNotEmpty(businessInterface.getRequestInterfaceId())
-						? businessInterface.getRequestInterfaceId() : "";
-
-				// 拼接实体
-				InterfaceDebugDto dto = new InterfaceDebugDto();
-				if ("2".equals(businessInterface.getSysIntfInParamFormatType())) {
-					String inparamFormat = sqlQueryFactory.select(qTInterface.inParamFormat).from(qTInterface).where(
-							qTInterface.id.eq(interfaceId).and(qTInterface.sysId.eq(businessInterface.getRequestSysId())))
-							.fetchFirst();
-					dto.setFormat(inparamFormat);
-
-					String wsUrl = niFiRequestUtil.getWsServiceUrl();
-					if (!wsUrl.endsWith("/")) {
-						wsUrl = wsUrl + "/";
-					}
-
-					// 获取医院名称列表
-					List<String> sysconfigIds = new ArrayList<>();
-					businessInterfaces.forEach(bi -> {
-						if (StringUtils.isNotEmpty(businessInterface.getRequestSysconfigId())) {
-							sysconfigIds.add(businessInterface.getRequestSysconfigId());
-						}
-						if (StringUtils.isNotEmpty(businessInterface.getRequestedSysconfigId())) {
-							sysconfigIds.add(businessInterface.getRequestedSysconfigId());
-						}
-					});
-					List<String> hospitalCodes = sqlQueryFactory.select(qTSysHospitalConfig.hospitalCode)
-							.from(qTSysHospitalConfig).leftJoin(qTSysConfig)
-							.on(qTSysConfig.id.eq(qTSysHospitalConfig.sysConfigId)).leftJoin(qTHospital)
-							.on(qTSysHospitalConfig.hospitalId.eq(qTHospital.id)).where(qTSysConfig.id.in(sysconfigIds))
-							.fetch();
-
-					String suffix = "services/" + businessInterface.getSysCode() + "/" + hospitalCodes.get(0);
-					wsUrl = wsUrl + suffix;
-					dto.setWsdlUrl(wsUrl);
-					List<String> wsOperationNames = PlatformUtil.getWsdlOperationNames(wsUrl);
-					dto.setWsOperationName(wsOperationNames.get(0));
-					dto.setSysIntfParamFormatType("2");
-					dto.setFuncode(businessInterface.getInterfaceUrl());
-				} else {
-					dto.setSysIntfParamFormatType("3");
-				}
-
-				if ("2".equals(dto.getSysIntfParamFormatType())) {
-					String wsdlUrl = dto.getWsdlUrl();
-					String methodName = dto.getWsOperationName();
-					String funcode = dto.getFuncode();
-					String param = dto.getFormat();
-					PlatformUtil.invokeWsService(wsdlUrl, methodName, funcode, param);
-				} else {
-					List<TLog> logs = sqlQueryFactory.select(qTLog).from(qTLog).where(qTLog.businessInterfaceId.eq(id)).fetch();
-					String format = "";
-					if(logs.size()>0){
-						TLog tLog = logs.get(0);
-						format = decryptAndFilterSensitive(tLog.getBusinessReq());
-					}
+				List<TLog> logs = sqlQueryFactory.select(qTLog).from(qTLog).where(qTLog.id.in(realIds)).fetch();
+				for(TLog tlog: logs) {
+					String format = decryptAndFilterSensitive(tlog.getBusinessReq());
 					niFiRequestUtil.interfaceDebug(format);
 				}
-//				successCount++;
 			} catch (Exception e) {
 				logger.error("获取接口调试显示数据失败! MSG:{}", ExceptionUtil.dealException(e));
 				return new ResultDto<>(Constant.ResultCode.ERROR_CODE, "日志重放失败");
-//				errorConut ++;
-//				errorIds += id + ",";
 			}
+		}
 		return new ResultDto<>(Constant.ResultCode.SUCCESS_CODE, "日志重放成功");
-//		}
-//		if(successCount == idArrays.length){
-//			return new ResultDto<>(Constant.ResultCode.SUCCESS_CODE, "日志重放成功",
-//					"成功条数为" +successCount + "");
-//		}else if(errorConut > 0 && successCount > 0){
-//			return new ResultDto<>(Constant.ResultCode.ERROR_CODE, "部分日志重放成功," + errorIds +"重放失败",
-//					"成功条数为" + successCount);
-//		}else{
-//			return new ResultDto<>(Constant.ResultCode.ERROR_CODE, "日志重放失败,"+ errorIds +"重放失败");
-//		}
 	}
 
 	/**
