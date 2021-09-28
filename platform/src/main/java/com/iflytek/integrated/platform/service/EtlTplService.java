303a89d3f9a79fec5db7a3a9a54023718d25c51a
leiwang81 <10885-leiwang81@users.noreply.git.iflytek.com>
2021-09-28 15:39:05 +0800

    Update EtlTplService.java

--- platform/src/main/java/com/iflytek/integrated/platform/service/EtlTplService.java
+++ platform/src/main/java/com/iflytek/integrated/platform/service/EtlTplService.java
@@ -469,6 +469,9 @@ public class EtlTplService extends BaseService<TEtlTpl, String, StringPath> {
 					String tplContent = IOUtils.toString(is, "UTF-8");
 					is.close();
 
+                    JSONObject jsonObject= JSONObject.parseObject(XmlJsonUtils.convertXmlToJsonObject(tplContent));
+                    String groupId=(String) jsonObject.getJSONObject("template").get("groupId");//获取模板中的groupId
+
 					file = new File(filename);
 					if (!file.exists()) {
 						file.createNewFile();
@@ -483,7 +486,7 @@ public class EtlTplService extends BaseService<TEtlTpl, String, StringPath> {
 					Type localVarReturnType = new TypeToken<String>(){}.getType();
 					
 					ApiResponse<String> resp = client.execute(call, localVarReturnType);
-					Map<String,String> tpl = parseResponse(resp.getData());
+					Map<String,String> tpl = parseResponse(resp.getData(),groupId);
 					uploadedTpls.add(tpl);
 				}
 			}catch (Exception e) {
@@ -538,7 +541,7 @@ public class EtlTplService extends BaseService<TEtlTpl, String, StringPath> {
 		 return call;
 	}
 	
-	private Map<String, String> parseResponse(String xml) throws DocumentException {
+	private Map<String, String> parseResponse(String xml,String groupId) throws DocumentException {
 		Map<String, String> result = new HashMap<>();
 		Document doc = null;
         doc = DocumentHelper.parseText(xml);
@@ -551,6 +554,7 @@ public class EtlTplService extends BaseService<TEtlTpl, String, StringPath> {
         	result.put("flowTplName", tplName);
         	result.put("flowTplId", tplId);
         	result.put("description", desp);
+			result.put("groupId",groupId);
         }
         return result;
 	}
