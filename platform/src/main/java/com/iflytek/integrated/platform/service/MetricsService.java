715a77d46407b0634b6bec639ad49113e4cc869a
unknown <kunfang6@iflytek.com>
2022-07-20 10:09:37 +0800

    grafana引入项目修改

--- platform/src/main/java/com/iflytek/integrated/platform/service/MetricsService.java
+++ platform/src/main/java/com/iflytek/integrated/platform/service/MetricsService.java
@@ -1,6 +1,5 @@
 package com.iflytek.integrated.platform.service;
 
-import com.iflytek.integrated.common.config.MetricsConfig;
 import com.iflytek.integrated.common.dto.ResultDto;
 import com.iflytek.integrated.platform.common.Constant;
 import io.swagger.annotations.Api;
@@ -9,11 +8,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @Slf4j
 @Api(tags = "服务监控管理")
@@ -22,12 +20,12 @@ import java.util.List;
 public class MetricsService {
     private static final Logger logger = LoggerFactory.getLogger(com.iflytek.integrated.platform.service.CdaService.class);
 
-    @Autowired
-    private MetricsConfig config;
+    @Value("${grafana}")
+    private String grafana;
 
-    @ApiOperation(value = "获取服务分类列表")
-    @GetMapping("/getUrlList")
-    public ResultDto<List<MetricsConfig.Metrics>> getMetrics() {
-        return new ResultDto<>(Constant.ResultCode.SUCCESS_CODE, "数据获取成功!", config.getList());
+    @ApiOperation(value = "获取监控地址")
+    @GetMapping("/getGrafanaUrl")
+    public ResultDto<String> getMetrics() {
+        return new ResultDto<>(Constant.ResultCode.SUCCESS_CODE, "数据获取成功!", grafana);
     }
 }
